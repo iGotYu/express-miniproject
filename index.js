840066aece0456213a4e17da68835e98f1d45093
Andrew Yu <igotyuu421@gmail.com>
2021-04-20 11:49:11 -0700

    pulling

--- index.js
+++ index.js
@@ -5,9 +5,12 @@ const PORT = 3000;
 const app = express();
 
 let tables = [];
+let wait = [];
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
+app.use(express.static("public"))
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname,"home.html"))
@@ -17,7 +20,30 @@ app.get("tables", (req, res) => {
     res.sendFile(path.join(__dirname, 'table.html'));
 })
 
-app.get("form", (req, res) => {
+app.get("wait", (req, res) => {
     res.sendFile(path.join(__dirname,'form.html'));
 })
 
+app.post ("/api/tables", (req, res) => {
+    res.json(tables);
+})
+
+app.post ("/api/wait", (req, res) => {
+    res.json(wait);
+})
+
+app.post ("/api/tables", (req, res) =>{
+    console.log (req.body.table);
+    tables.push(req.body.table);
+    res.send(req.body.table);
+})
+
+app.post ("/api/form", (req, res) =>{
+    console.log (req.body.form);
+    wait.push(req.body.form);
+    res.send(req.body.form);
+})
+
+app.listen(PORT,()=>{
+    console.log("listening on port" + PORT)
+})
